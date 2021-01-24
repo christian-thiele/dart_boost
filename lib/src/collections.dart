da27bfb6864a33caafd96c63683cd126458bdfa1
Christian Thiele <christian.thiele@corelightservices.com>
2021-01-24 19:03:45 +0100

    basic structures, boost exception, distinct, groupBy, sequenceEquals, split, zip, replaceItem, sortBy

--- lib/src/collections.dart
+++ lib/src/collections.dart
@@ -1,7 +1,11 @@
 import 'dart:math';
 
-/// selector / test delegates for methods like where(), any(), etc.
+import 'package:boost/src/exceptions.dart';
+
+import 'structures.dart';
+
 bool notNull(e) => e != null;
+
 bool isNull(e) => e == null;
 
 extension NullableIterableExtension<TValue> on Iterable<TValue?> {
@@ -9,13 +13,115 @@ extension NullableIterableExtension<TValue> on Iterable<TValue?> {
   Iterable<TValue> get whereNotNull => where(notNull).map((e) => e!);
 }
 
-extension IterableUtils<TValue> on Iterable<TValue> {
-  /// picks a random element of the iterable
+extension IterableExtension<TValue> on Iterable<TValue> {
+  /// Returns the first element of the iterable if not empty, null otherwise.
+  TValue? get firstOrNull => isNotEmpty ? first : null;
+
+  /// Returns the last element of the iterable if not empty, null otherwise.
+  TValue? get lastOrNull => isNotEmpty ? last : null;
+
+  /// Returns a random element.
   TValue get random {
     if (isEmpty) {
       return first; // supposed to trigger internal noElement exception
     }
 
     return elementAt(Random().nextInt(length));
+  }
+
+  /// Returns a subset of this [Iterable] containing every element whose key is
+  /// not equal to the key of any previous element.
+  ///
+  /// If [keySelector] is null, the element itself is considered as key.
+  Iterable<TValue> distinct([Function(TValue)? keySelector]) sync* {
+    final keys = [];
+    for (final element in this) {
+      final key = (keySelector == null) ? element : keySelector(element);
+      if (keys.contains(key)) continue;
+      keys.add(key);
+      yield element;
+    }
+  }
+
+  /// Groups elements of this [iterable] by comparing the [selector] result.
+  Map<TKey, List<TValue>> groupBy<TKey>(TKey Function(TValue) selector) {
+    final map = <TKey, List<TValue>>{};
+    forEach((element) {
+      final key = selector(element);
+      if (map[key] == null) {
+        map[key] = [element];
+      } else {
+        map[key]!.add(element);
+      }
+    });
+    return map;
+  }
+
+  /// Deep equality check.
+  ///
+  /// Checks if elements in [other] are equal to the elements in this [Iterable].
+  bool sequenceEquals(Iterable<TValue> other) {
+    if (this == other) {
+      return true;
+    }
+
+    if (other.length != length) {
+      return false;
+    }
+
+    for (final pair in zip(other)) {
+      if (pair.a != pair.b) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  /// Splits the collection into two subsets.
+  ///
+  /// Every element for which [selector] returns true is added to the
+  /// [Tuple.a] list, otherwise it is added to the [Tuple.b] list.
+  Tuple<List<TValue>, List<TValue>> split(bool Function(TValue) selector) {
+    final collections = Tuple<List<TValue>, List<TValue>>([], []);
+    forEach((element) {
+      final collection = selector(element) ? collections.a : collections.b;
+      collection.add(element);
+    });
+    return collections;
+  }
+
+  /// Merges every element of this [Iterable] with the corresponding element
+  /// of [other] into a [Tuple].
+  ///
+  /// If this and [other] have varying lengths, null will be substituted for
+  /// the missing counter part in the result [Tuple].
+  Iterable<Tuple<TValue?, TOther?>> zip<TOther>(Iterable<TOther> other) {
+    return Iterable.generate(
+        max(length, other.length),
+            (i) => Tuple(length > i ? elementAt(i) : null,
+            other.length > i ? other.elementAt(i) : null));
+  }
+}
+
+extension ListExtension<TValue> on List<TValue> {
+  /// Replaces a single element of this [List] with [replacement].
+  ///
+  /// Removes the first object which is equal to [needle],
+  /// then inserts [replacements] at the former index of [needle].
+  void replaceItem(TValue needle, TValue replacement, [int start = 0]) {
+    final idx = indexOf(needle, start);
+    if (idx == -1) {
+      throw BoostException('Item not found in list.');
+    }
+    replaceRange(idx, idx + 1, [replacement]);
+  }
+
+  /// Sorts this [List] by comparing the keys returned by the [selector].
+  void sortBy<TKey extends Comparable>(TKey Function(TValue) selector,
+      [bool ascending = true]) {
+    sort((a, b) => ascending
+        ? Comparable.compare(selector(a), selector(b))
+        : Comparable.compare(selector(b), selector(a)));
   }
 }
--- test/boost_test.dart
+++ test/boost_test.dart
@@ -1,10 +1,25 @@
+import 'dart:math';
+
+import 'package:boost/src/structures.dart';
 import 'package:test/test.dart';
 import 'package:boost/boost.dart';
 
 void main() {
+  group('Structures', () {
+    test('Tuple', _tupleTest);
+    test('Triple', _tripleTest);
+  });
+
   group('Collections', () {
     test('whereNotNull', _whereNotNullTest);
     test('random', _randomTest);
+    test('distinct', _distinctTest);
+    test('groupBy', _groupByTest);
+    test('sequenceEquals', _sequenceEqualsTest);
+    test('split', _splitTest);
+    test('zip', _zipTest);
+    test('replaceItem', _replaceItemTest);
+    test('sortBy', _sortByTest);
   });
 
   group('Types', () {
@@ -13,6 +28,86 @@ void main() {
     test('isMap', _isMapTest);
     test('isMapOfType', _isMapOfTypeTest);
   });
+}
+
+// Structures
+void _tupleTest() {
+  final tuple = Tuple(5, 'text');
+  expect(tuple.a, equals(5));
+  expect(tuple.b, equals('text'));
+
+  final withA = tuple.withA(9);
+  expect(withA.a, equals(9));
+  expect(withA.b, equals('text'));
+
+  final withB = tuple.withB('other');
+  expect(withB.a, equals(5));
+  expect(withB.b, equals('other'));
+
+  // equality comparator
+  expect(tuple, equals(tuple));
+  expect(tuple, isNot(equals(withA)));
+  expect(tuple, isNot(equals(withB)));
+
+  expect(withA, isNot(equals(tuple)));
+  expect(withA, equals(withA));
+  expect(withA, isNot(equals(withB)));
+
+  expect(withB, isNot(equals(tuple)));
+  expect(withB, isNot(equals(withA)));
+  expect(withB, equals(withB));
+
+  expect(tuple, equals(withA.withA(tuple.a)));
+  expect(tuple, equals(withB.withB(tuple.b)));
+  expect(withB.withA(withA.a), equals(withA.withB(withB.b)));
+}
+
+void _tripleTest() {
+  final dt = DateTime.now();
+  final dt2 = dt.add(Duration(days: 1));
+
+  final triple = Triple(0.4, dt, false);
+  expect(triple.a, equals(0.4));
+  expect(triple.b, equals(dt));
+  expect(triple.c, equals(false));
+
+  final withA = triple.withA(9);
+  expect(withA.a, equals(9.0));
+  expect(withA.b, equals(dt));
+  expect(withA.c, equals(false));
+
+  final withB = triple.withB(dt2);
+  expect(withB.a, equals(0.4));
+  expect(withB.b, equals(dt2));
+  expect(withB.c, equals(false));
+
+  final withC = triple.withC(true);
+  expect(withC.a, equals(0.4));
+  expect(withC.b, equals(dt));
+  expect(withC.c, equals(true));
+
+  // equality comparator
+  expect(triple, equals(triple));
+  expect(triple, isNot(equals(withA)));
+  expect(triple, isNot(equals(withB)));
+  expect(triple, isNot(equals(withC)));
+
+  expect(withA, isNot(equals(triple)));
+  expect(withA, equals(withA));
+  expect(withA, isNot(equals(withB)));
+  expect(withA, isNot(equals(withC)));
+
+  expect(withB, isNot(equals(triple)));
+  expect(withB, isNot(equals(withA)));
+  expect(withB, equals(withB));
+  expect(withB, isNot(equals(withC)));
+
+  expect(triple, equals(withA.withA(triple.a)));
+  expect(triple, equals(withB.withB(triple.b)));
+  expect(triple, equals(withC.withC(triple.c)));
+
+  expect(withB.withA(withA.a), equals(withA.withB(withB.b)));
+  expect(withC.withA(withA.a), equals(withA.withC(withC.c)));
 }
 
 // Collections
@@ -28,6 +123,99 @@ void _randomTest() {
   final firstResult = results.first;
 
   expect(results, isNot(everyElement(equals(firstResult))));
+}
+
+void _distinctTest() {
+  final text = 'abc def ghi def abc jkl mno ghi';
+  final distinct = text.split(' ').distinct();
+  expect(distinct, ['abc', 'def', 'ghi', 'jkl', 'mno']);
+
+  final list = [
+    Tuple(1, 'first 1'),
+    Tuple(2, 'first 2'),
+    Tuple(1, 'second 1'),
+    Tuple(3, 'first 3')
+  ];
+  expect(list.distinct((e) => e.a).map((e) => e.b),
+      ['first 1', 'first 2', 'first 3']);
+}
+
+void _groupByTest() {
+  final people = ['Joe', 'Alex', 'Grace', 'Tina', 'Max'];
+  final groups = people.groupBy((e) => e.length);
+  expect(groups, equals({
+    3: ['Joe', 'Max'],
+    4: ['Alex', 'Tina'],
+    5: ['Grace']
+  }));
+}
+
+void _sequenceEqualsTest() {
+  final seq1 = [1, 2, 5, 7, 9, 20];
+  final seq2 = [1, 2, 5, 7, 9, 20];
+  final seq3 = [1, 2, 5, 7, 20, 9];
+  final seq4 = [1, 2, 5, 7];
+
+  expect(seq1.sequenceEquals(seq1), isTrue);
+  expect(seq1.sequenceEquals(seq2), isTrue);
+  expect(seq1.sequenceEquals(seq3), isFalse);
+  expect(seq3.sequenceEquals(seq1), isFalse);
+  expect(seq3.sequenceEquals(seq2), isFalse);
+  expect(seq1.sequenceEquals(seq4), isFalse);
+}
+
+void _splitTest() {
+  final people = [
+    Tuple('Joe', false),
+    Tuple('Alex', true),
+    Tuple('Grace', true),
+    Tuple('Tina', false),
+    Tuple('Max', false),
+  ];
+
+  final split = people.split((p) => p.b);
+  expect(split.a, equals(people.where((p) => p.b)));
+  expect(split.b, equals(people.where((p) => !p.b)));
+}
+
+void _zipTest() {
+  final seq1 = ['a', 'b', 'c', 'd'];
+  final seq2 = [5, 4, 3, 2, 1, 0];
+  final combined = seq1.zip(seq2);
+
+  expect(
+      combined,
+      equals(<Tuple<String?, int?>>[
+        Tuple('a', 5),
+        Tuple('b', 4),
+        Tuple('c', 3),
+        Tuple('d', 2),
+        Tuple(null, 1),
+        Tuple(null, 0),
+      ]));
+}
+
+void _replaceItemTest() {
+  final seq = <dynamic>['abc', 2, 0.4, false];
+  seq.replaceItem(0.4, 'def');
+  expect(seq, equals(['abc', 2, 'def', false]));
+}
+
+void _sortByTest() {
+  final list = [
+    Tuple('g', 5),
+    Tuple('e', 2),
+    Tuple('u', 8),
+    Tuple('a', 3),
+    Tuple('f', 10),
+    Tuple('i', 4),
+  ];
+
+  list.sortBy((e) => e.a);
+  expect(list.map((e) => e.b), [3, 2, 10, 5, 4, 8]);
+
+  list.sortBy((e) => e.b);
+  expect(list.map((e) => e.b), [2, 3, 4, 5, 8, 10]);
 }
 
 // Types
