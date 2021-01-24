db5a819d1ed0851ed868077541609488263d835d
Christian Thiele <christian.thiele@corelightservices.com>
2021-01-24 19:04:23 +0100

    split example

--- example/boost_example.dart
+++ example/boost_example.dart
@@ -1,5 +1,25 @@
 import 'package:boost/boost.dart';
 
+class Person {
+  final String name;
+  final bool clubMember;
+
+  Person(this.name, this.clubMember);
+}
+
 void main() {
-  // TODO write example
+  final people = [
+    Person('Joe', false),
+    Person('Alex', true),
+    Person('Grace', true),
+    Person('Tina', false),
+    Person('Max', false),
+  ];
+
+  final split = people.split((p) => p.clubMember);
+  print('${split.a.length} people are club Members:');
+  split.a.forEach((p) => print(p.name));
+
+  print('${split.b.length} people are not:');
+  split.b.forEach((p) => print(p.name));
 }
