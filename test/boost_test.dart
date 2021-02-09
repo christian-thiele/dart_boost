cae6face666163208c1b3bf66b474b96ae20dc57
Christian Thiele <christian.thiele@corelightservices.com>
2021-02-09 11:16:29 +0100

    added test cases to min/max added round method in math section

--- test/boost_test.dart
+++ test/boost_test.dart
@@ -36,6 +36,10 @@ void main() {
     test('isMapOfType', _isMapOfTypeTest);
   });
 
+  group('Math', () {
+    test('round', _roundTest);
+  });
+
   group('Concurrency', () {
     test('runGuarded', _runGuardedTest);
     test('cancelOn', _cancelOnTest);
@@ -310,6 +314,26 @@ void _isMapOfTypeTest() {
   expect(intDoubleMap.runtimeType.isMapOfType<double, int>(), isFalse);
   expect(intDoubleMap.runtimeType.isMapOfType<int, int>(), isFalse);
 }
+
+// Math
+void _roundTest() {
+  expect(round(0), equals(0));
+  expect(round(10), equals(10));
+  expect(round(10, -2), equals(0));
+  expect(round(10, 2), equals(10));
+  expect(round(1.25, 1), equals(1.3));
+  expect(round(-1.25, 1), equals(-1.3));
+  expect(round(545, -1), equals(550));
+  expect(round(545, -2), equals(500));
+  expect(round(545, 0), equals(545));
+  expect(round(545, 2), equals(545));
+  expect(round(545, -3), equals(1000));
+
+  expect(round(double.infinity, 2), equals(double.infinity));
+  expect(round(double.negativeInfinity, 2), equals(double.negativeInfinity));
+  expect(round(double.nan, 2), isNaN);
+}
+
 
 // Concurrency
 Future _runGuardedTest() async {
